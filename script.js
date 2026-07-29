71c86aa7060fe039c481a0e89a2d7cb69df92ba9
yasa-iky <rizqnanda.live@gmail.com>
2026-07-30 02:33:43 +0700

    Upload website AL MALSYARI Coffee

---
+++ script.js
@@ -0,0 +1,16 @@
+const navbarLinks = document.querySelectorAll(".nav-menu .nav-link");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+  // Toggle mobile menu visibility
+  document.body.classList.toggle("show-mobile-menu");
+});
+
+// Close menu when the close button is clicked
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+// Close menu when nav link is clicked
+navbarLinks.forEach((link) => {
+  link.addEventListener("click", () => menuOpenButton.click());
+});
